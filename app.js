1512584c547ac931815dbc76532eb6f8e12eb8fd
LudensDK <146302258+LudensDK@users.noreply.github.com>
2024-05-12 00:01:20 +0900

    app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function startProcess() {
 
     const minRating = parseFloat(document.getElementById('min-rating').value);
 
-    fetch('data/soccer_players.csv') // CSVファイルを読み込む
+    fetch('data/playersdata.csv') // CSVファイルを読み込む
         .then(response => response.text())
         .then(csvText => {
             const rows = csvText.split('\n').map(row => row.split(','));
